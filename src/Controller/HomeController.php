b98baa2bb2fce1030a73cb896a32a7b59806d966
DankestDev <DankestMirror@hotmail.es>
2024-11-06 13:59:13 +0000

    Done basic

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class HomeController extends AbstractController
 {
@@ -19,6 +20,18 @@ class HomeController extends AbstractController
     public function dashboard(): Response
     {
         return $this->render('home/dashboard.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route('/admin', name: 'admin')]
+    public function admin(AuthorizationCheckerInterface $authChecker): Response
+    {
+        if (!$authChecker->isGranted('ROLE_ADMIN')) {
+            return $this->render('home/index.html.twig', [
+                'controller_name' => 'HomeController',
+            ]);
+         }
+        return $this->render('home/admin.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
